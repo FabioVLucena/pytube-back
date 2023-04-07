6f522a3badc4fdabcc59a138a421ff34d68043a1
aeon <yasunofabio@outlook.com.br>
2023-04-07 15:18:54 -0400

    remove scripts de teste

--- playlist.py
+++
@@ -1,7 +0,0 @@
-from pytube import Playlist
-
-playlist = Playlist("https://www.youtube.com/watch?v=zmxJbz-o-Ak&list=PLT9lZaywI5etSYh7EqkeKB0E_8cqTN1w_");
-print('Number of videos in playlist: %s' % len(playlist.video_urls))
-
-for video in playlist.videos :
-    video.streams.get_audio_only().download(output_path="./videos")
--- video.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from pytube import YouTube
-
-yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
-# stream = yt.streams.get_audio_only()
-teste = yt.watch_url
-print(teste)
-
-# stream.download(output_path="./videos")
